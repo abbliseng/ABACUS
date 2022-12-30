3e77e9484f1e7b39ca7556e1bf3541e559ac4223
abbliseng <54662679+abbliseng@users.noreply.github.com>
2022-12-30 13:25:02 +0100

    page loading v1

--- hemsida/main.js
+++ hemsida/main.js
@@ -1,5 +1,24 @@
 const pages = ['home-page', 'login-page', 'profile-page']
 
+function show_page(id) {
+    document.getElementById(id).style.display = 'block'
+}
+
+function hide_page(id) {
+    document.getElementById(id).style.display = 'none'
+}
+
+function hide_all_but_one(id) {
+    for (i in pages) {
+        if (pages[i] === id) {
+            show_page(pages[i])
+        } else {
+            hide_page(pages[i])
+        }
+    }
+}
+
+
 // FIXME: VERY insecure, need to build some confidence!
 function get_login_stats() {
     return false
@@ -7,6 +26,14 @@ function get_login_stats() {
 // FIXME: VERY insecure, need to build some confidence!
 function show_profile() {
     if (!get_login_stats()) {
-
+        hide_all_but_one(pages[id])
     }
 }
+
+
+
+// MAIN
+// Only runs once page has fully loaded
+window.addEventListener('load', function () {
+    hide_all_but_one('home-page')
+})
