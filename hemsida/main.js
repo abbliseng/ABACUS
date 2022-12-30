f3e1a199314064bf0b405e3dc459c8f2914260d2
abbliseng <54662679+abbliseng@users.noreply.github.com>
2022-12-30 12:26:01 +0100

    toolbar v1

---
+++ hemsida/main.js
@@ -0,0 +1,12 @@
+const pages = ['home-page', 'login-page', 'profile-page']
+
+// FIXME: VERY insecure, need to build some confidence!
+function get_login_stats() {
+    return false
+}
+// FIXME: VERY insecure, need to build some confidence!
+function show_profile() {
+    if (!get_login_stats()) {
+
+    }
+}
